ba99f2886ac6af9a00a60ca8f89b41b707e55c1d
prashan <prashansilva@gmail.com>
2025-09-22 11:22:21 +0530

    Module Federation Angular Host

--- remote-app/src/mount.tsx
+++
@@ -1,8 +0,0 @@
-import ReactDOM from "react-dom/client";
-import App from "./App";
-
-export function mount(element: HTMLElement) {
-    const root = ReactDOM.createRoot(element);
-    root.render(<App />);
-    return () => root.unmount();
-}
